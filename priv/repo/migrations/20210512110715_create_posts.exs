f3d6ec03daf95b1af217054befbfbc8f99fb0c66
SS-FS-58 <lada1126@outlook.com>
2021-05-12 20:14:28 +0800

    first twitter sample with Phoenix liveview

---
+++ priv/repo/migrations/20210512110715_create_posts.exs
@@ -0,0 +1,15 @@
+defmodule Chirp.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :username, :string
+      add :body, :string
+      add :likes_count, :integer
+      add :reposts_counter, :integer
+
+      timestamps()
+    end
+
+  end
+end
